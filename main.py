f7d43c884976bf79fd571cf14a5aa72c616fc100
Kacper Motyka <kmotyka@student.agh.edu.pl>
2021-11-21 19:42:46 +0100

    SA algorithm

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 from typing import List
 from enum import Enum
-
+import copy
+import random
+import cv2
 
 class LessonType(Enum):
     """ Class enumerating lesson types,
@@ -51,7 +53,10 @@ class Lesson:
             self.participants = np.array(participants)
 
     def __str__(self):
-        return f"I: {self.instructor.id}, L: {self.lesson_type}"
+        lesson_type = str(self.lesson_type)
+        lt = lesson_type.split(sep='.')
+        lesson_type = lt[1]
+        return f"Instructor Id: {self.instructor.id}, Type: {lesson_type}"
 
 
 class Schedule:
@@ -88,42 +93,49 @@ class Schedule:
         self.pay_for_presence = pay_for_presence
         self.class_renting_cost = class_renting_cost
 
-    def generate_random_schedule(self):
-        # picked = [lesson_type for client in self.clients for lesson_type in client.selected_training]
-        # type_and_count = {}
-        # for lesson_type in LessonType:
-        #     type_and_count[lesson_type] = picked.count(lesson_type)
+    def generate_random_schedule(self, greedy=False):
 
-        # temp reshaping for easier indexing
-        self.schedule = self.schedule.reshape((-1, 1))
+        self.schedule = self.schedule.reshape((-1, 1))  # temp reshaping for easier indexing
         i = 0
+        free_ts = list(np.arange(self.schedule.shape[0]))
         for lesson_type in LessonType:
             all_participants = [client for client in self.clients if lesson_type in client.selected_training]
             num_of_trainings = int(np.ceil(len(all_participants) / self.max_clients_per_training))
             for training in range(num_of_trainings):
-                instructor = self.instructors[i % self.instructors.shape[0]]
                 participants = all_participants[training * self.max_clients_per_training:
                                                 training * self.max_clients_per_training + self.max_clients_per_training]
-                self.schedule[i] = Lesson(instructor, lesson_type, participants)
-                i += 1
-        self.schedule = self.schedule.reshape((self.class_id.shape[0], self.day.shape[0], self.time_slot.shape[0]))
-        i = 0
 
-    def goal_function(self):
-        # dla każdej komórki w self.schedule policzyć liczbę uczestników,
+                if greedy:
+                    instructor = self.instructors[i % self.instructors.shape[0]]
+                    self.schedule[i] = Lesson(instructor, lesson_type, participants)
+                    i += 1
+
+                else:
+                    random_id = free_ts.pop(random.randint(0, len(free_ts) - 1))
+                    instructor = self.instructors[random_id % self.instructors.shape[0]]
+                    self.schedule[random_id] = Lesson(instructor, lesson_type, participants)
+
+        self.schedule = self.schedule.reshape((self.class_id.shape[0], self.day.shape[0], self.time_slot.shape[0]))
+
+    def get_cost(self, current_solution=None):
+        # dla każdej komórki w current_solution policzyć liczbę uczestników,
         # pomnożyć przez cenę zajęć,
         # odjąć koszt przyjścia trenera w danym dniu
         # i odjąć koszt wynajęcia sali w danym dniu
+
+        if current_solution is None:
+            current_solution = self.schedule
+
         participants_sum = 0
         instructors_hours = np.zeros(shape=(self.instructors.shape[0], self.day.shape[0]))
         class_per_day = np.zeros(shape=(self.class_id.shape[0], self.day.shape[0]))
 
-        for c in range(self.schedule.shape[0]):
-            for d in range(self.schedule.shape[1]):
-                for ts in range(self.schedule.shape[2]):
-                    if self.schedule[c, d, ts] is not None:
-                        participants_sum += self.schedule[c, d, ts].participants.shape[0]
-                        instructors_hours[self.schedule[c, d, ts].instructor.id, d] += 1
+        for c in range(current_solution.shape[0]):
+            for d in range(current_solution.shape[1]):
+                for ts in range(current_solution.shape[2]):
+                    if current_solution[c, d, ts] is not None:
+                        participants_sum += current_solution[c, d, ts].participants.shape[0]
+                        instructors_hours[current_solution[c, d, ts].instructor.id, d] += 1
                         class_per_day[c, d] = 1
 
         return self.ticket_cost * participants_sum - \
@@ -131,11 +143,100 @@ class Schedule:
                self.pay_for_presence * (instructors_hours > 0).sum() - \
                self.class_renting_cost * class_per_day.sum()
 
+
+    def get_neighbor(self, current_solution):
+
+        not_none_id_list = np.argwhere(current_solution != None)
+        random_not_none_id = tuple(random.choice(not_none_id_list))
+
+        none_id_list = np.argwhere(current_solution == None)
+        random_none_id = tuple(random.choice(none_id_list))
+
+        current_solution[random_none_id] = current_solution[random_not_none_id]
+        current_solution[random_not_none_id] = None
+        return current_solution
+
+    def simulated_annealing(self, alpha=0.999, initial_temp=1000, n_iter_one_temp=5, min_temp=0.1, epsilon=0.1,
+                            n_iter_without_improvement=1000, initial_solution=False):
+
+        if not initial_solution:
+            self.generate_random_schedule()  # self.schedule initialized
+
+        current_solution = copy.deepcopy(self.schedule)
+        best_solution = copy.deepcopy(current_solution)
+
+        current_temp = initial_temp
+
+        current_cost = self.get_cost(current_solution)
+        best_cost = self.get_cost(best_solution)
+
+        counter = 0
+        total_counter = 0
+
+        while current_temp > min_temp and counter < n_iter_without_improvement:
+            for j in range(0, n_iter_one_temp):
+                total_counter += 1
+                neighbor_solution = self.get_neighbor(current_solution)
+                neighbor_cost = self.get_cost(neighbor_solution)
+                delta = neighbor_cost - current_cost
+                if delta >= 0:
+                    current_solution = neighbor_solution
+                    current_cost = neighbor_cost
+                    if current_cost > best_cost:
+                        best_solution = copy.deepcopy(current_solution)
+                        best_cost = current_cost
+                else:
+                    s = random.uniform(0, 1)
+                    if s < np.exp(delta/current_temp):
+                        current_solution = neighbor_solution
+                        current_cost = neighbor_cost
+
+            current_temp = alpha * current_temp
+
+            if (abs(current_cost - best_cost)) < epsilon:
+                counter += 1
+            else:
+                counter = 0
+
+        self.schedule = best_solution
+        return best_cost, total_counter
+
     def __str__(self):
-        return str(self.schedule)
+
+        result = str()
+        days = ["-- MONDAY --", "-- TUESDAY --", "-- WEDNESDAY --", "-- THURSDAY --", "-- FRIDAY --", "-- SATURDAY --"]
+        hour = ["16:00 - 17:00", "17:00 - 18:00", "18:00 - 19:00", "19:00 - 20:00", "20:00 - 21:00", "21:00 - 22:00"]
+        for c in range(self.schedule.shape[0]):
+            for d in range(self.schedule.shape[1]):
+                result += "\n" + days[d] + "\n\n"
+                for ts in range(self.schedule.shape[2]):
+                    result += hour[ts] + "\t"
+                    if self.schedule[c, d, ts] is None:
+                        result += "Free\n"
+                    else:
+                        result += str(self.schedule[c, d, ts]) + "\n"
+
+        return result
 
 
-SM = Schedule()
-SM.generate_random_schedule()
-print(SM.goal_function())
-print("Essa")
+SM = Schedule(max_clients_per_training=2)
+
+SM.generate_random_schedule(greedy=False)
+print("INITIAL SCHEDULE")
+print(SM)
+print('Initial cost: ', SM.get_cost())
+
+best_cost, num_of_iter = SM.simulated_annealing(alpha=0.9999, initial_temp=1000, n_iter_one_temp=10, min_temp=0.1,
+                                                epsilon=0, n_iter_without_improvement=1000, initial_solution=True)
+
+print("AFTER OPTIMIZATION")
+print(SM)
+print("Number of iterations: ", num_of_iter)
+print("Best cost", best_cost)
+
+print("\nEssa")
+
+#  TODO: - poprawienie rozwiązania podczas działania algorytmu SA (przenoszenie względem prowadzących)
+#  TODO: - dodanie listy kompetencji i mniej losowe przydzielanie prowadzących do zajęć (może jako prawdopodobieństwo)
+#  TODO: - ograniczenia
+#  TODO: - ogarnąć co już zaimplemetnowaliśmy (jakie ograniczenia już są)
