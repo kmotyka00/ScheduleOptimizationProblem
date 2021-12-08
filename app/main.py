7e87a6844eb20c90b8797c988a74b0fef2a3e5f3
Kacper Motyka <kmotyka@student.agh.edu.pl>
2021-12-08 19:46:47 +0100

    start making GUI

---
+++ app/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+import kivy.clock
+
+import webbrowser
+import threading
+
+class MainWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('new_window.kv')
+
+class ScheduleOrganizer(App):
+    def build(self):
+        return kv
+
+
+if __name__ == '__main__':
+    ScheduleOrganizer().run()
